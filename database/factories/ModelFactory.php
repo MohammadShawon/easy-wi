461417aeef6a880efc458d925d131f8f3336e740
Ulrich Block <ulblock@gmx.de>
2017-06-10 13:26:03 +0200

    Add seeder for testing

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,17 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Admin::class, function (Faker\Generator $faker) {
+
     static $password;
 
     return [
         'name' => $faker->name,
+        'first_name' => $faker->name,
+        'last_name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'locked' => false,
         'remember_token' => str_random(10),
     ];
 });
